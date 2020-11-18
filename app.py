f505b3889a2264b379f16583751e3aa96e02741c
김재원 <ashwon12@naver.com>
2020-11-19 03:19:47 +0900

    add DB에 데이터 검색 기능

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.myCerti  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 
 # 처음 시작할 때 호출
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/listPage')
+def listPage():
+    major = request.args.get('major')
+    return render_template('listPage.html', major=major)
+
+
+# 검색 결과를 보여준다.
+@app.route('/result', methods=['POST'])
+def result_post():
+    keyword = request.form['major_give']
+
+    search_result = list(db.certificate.find({'certi': {'$regex': '.*' + keyword + '.*'}}, {'_id': False}))
+    results = []
+    for result in search_result:
+        results.append(result)
+
+    return jsonify({'result': 'success', 'data': results})
 
 
 if __name__ == '__main__':
